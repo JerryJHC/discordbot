3ecb3c51bcccf45a34062d2cb6c7c503ad7649da
Jerry Henríquez <jerryhenriquez@gmail.com>
2020-04-08 16:37:12 +0100

    added ytsr to perform youtube search if a link is not provided, first video is selected, removed ban and purge commands, and modified message to use util setMessage

--- commands/play.js
+++ commands/play.js
@@ -1,4 +1,5 @@
 const ytdl = require("ytdl-core");
+const ytsr = require("ytsr");
 const { setMessage } = require('../util/message');
 
 module.exports = {
@@ -6,7 +7,7 @@ module.exports = {
   description: "Play a song in your channel!",
   async execute(message) {
     const args = message.content.trim().split(/ +/g);
-    console.log(args);
+    if (args.length < 2) return message.channel.send(setMessage("A search string or youtube link is mandatory!"));
     try {
       const queue = message.client.queue;
       const serverQueue = message.client.queue.get(message.guild.id);
@@ -19,50 +20,54 @@ module.exports = {
         return message.channel.send(setMessage("I need the permissions to join and speak in your voice channel!"));
       }
 
-      if (ytdl.validateURL(args[1])) {
-        const songInfo = await ytdl.getInfo(args[1]);
-        if ((songInfo.length_seconds / 60) > 10) {
-          console.log(`${songInfo.title} is longer than 10 minutes!`);
-          return message.channel.send(setMessage(`**${songInfo.title}** is longer than 10 minutes!`));
+      let link = args[1];
+      if (!ytdl.validateURL(link)) {
+        let options = { limit: 1 };
+        args.shift();
+        let s = await ytsr(args.join(' '), options);
+        if (s.items.length > 0) {
+          link = s.items[0].link;
         }
-        const song = {
-          title: songInfo.title,
-          url: songInfo.video_url,
-          length: songInfo.length_seconds,
-          requester: message.member.user.username
+      }
+      const songInfo = await ytdl.getInfo(link);
+      if ((songInfo.length_seconds / 60) > 10) {
+        console.log(`${songInfo.title} is longer than 10 minutes!`);
+        return message.channel.send(setMessage(`**${songInfo.title}** is longer than 10 minutes!`));
+      }
+      const song = {
+        title: songInfo.title,
+        url: songInfo.video_url,
+        length: songInfo.length_seconds,
+        requester: message.member.user.username
+      };
+      console.log(song);
+
+      if (!serverQueue) {
+        const queueContruct = {
+          textChannel: message.channel,
+          voiceChannel: voiceChannel,
+          connection: null,
+          songs: [],
+          volume: 5,
+          playing: true
         };
-        console.log(song);
 
-        if (!serverQueue) {
-          const queueContruct = {
-            textChannel: message.channel,
-            voiceChannel: voiceChannel,
-            connection: null,
-            songs: [],
-            volume: 5,
-            playing: true
-          };
+        queue.set(message.guild.id, queueContruct);
 
-          queue.set(message.guild.id, queueContruct);
+        queueContruct.songs.push(song);
 
-          queueContruct.songs.push(song);
-
-          try {
-            var connection = await voiceChannel.join();
-            queueContruct.connection = connection;
-            this.play(message, queueContruct.songs[0]);
-          } catch (err) {
-            console.log(err);
-            queue.delete(message.guild.id);
-            return message.channel.send(setMessage(err));
-          }
-        } else {
-          serverQueue.songs.push(song);
-          return message.channel.send(setMessage(`${song.title} has been added to the queue!`));
+        try {
+          var connection = await voiceChannel.join();
+          queueContruct.connection = connection;
+          this.play(message, queueContruct.songs[0]);
+        } catch (err) {
+          console.log(err);
+          queue.delete(message.guild.id);
+          return message.channel.send(setMessage(err));
         }
       } else {
-        console.log("Search by name");
-        message.channel.send(setMessage('Sorry, I can only play songs through youtube links! but someday I could do more (-.-)/'));
+        serverQueue.songs.push(song);
+        return message.channel.send(setMessage(`**${song.title}** has been added to the queue!\nRequested by: ${song.requester}`));
       }
     } catch (error) {
       console.log(error);
--- commands/skip.js
+++ commands/skip.js
@@ -1,10 +1,12 @@
+const { setMessage } = require('../util/message');
+
 module.exports = {
 	name: 'skip',
 	description: 'Skip a song!',
 	execute(message) {
 		const serverQueue = message.client.queue.get(message.guild.id);
-		if (!message.member.voice.channel) return message.channel.send('You have to be in a voice channel to stop the music!');
-		if (!serverQueue) return message.channel.send('There is no song that I could skip!');
+		if (!message.member.voice.channel) return message.channel.send(setMessage('You have to be in a voice channel to stop the music!'));
+		if (!serverQueue) return message.channel.send(setMessage('There is no song that I could skip!'));
 		serverQueue.connection.dispatcher.end();
 	},
 };
