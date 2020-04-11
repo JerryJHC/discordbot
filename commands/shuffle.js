30e8ade3779f4c4daee1c7e95c29c6dc55e1bd59
Jerry Henríquez <jerryhenriquez@gmail.com>
2020-04-11 11:43:56 +0100

    added command to randomize queue except current song

---
+++ commands/shuffle.js
@@ -0,0 +1,20 @@
+const { setMessage } = require('../util/message');
+
+module.exports = {
+    name: 'shuffle',
+    description: 'Randomize all songs in the queue.',
+    execute(message) {
+        const serverQueue = message.client.queue.get(message.guild.id);
+        if (!serverQueue) return message.channel.send(setMessage('There is nothing playing.'));
+        let queue = serverQueue.songs.splice(1, serverQueue.songs.length);
+        this.shuffleArray(queue);
+        queue.forEach(v => serverQueue.songs.push(v));
+        return message.channel.send(setMessage(`${queue.length} songs has been shuffled`));
+    },
+    shuffleArray(array) {
+        for (let i = array.length - 1; i > 0; i--) {
+            const j = Math.floor(Math.random() * (i + 1));
+            [array[i], array[j]] = [array[j], array[i]];
+        }
+    }
+};
