0a66bd6699d8020d0cdf28c0ee6eb95c54dd4cb5
Jerry Henríquez <jerryhenriquez@gmail.com>
2020-04-11 13:06:09 +0100

    modified to detect if time it is in seconds or not

--- util/formatStr.js
+++ util/formatStr.js
@@ -1,5 +1,8 @@
 exports.getTime = (time) => {
-    let minutes = Math.floor(time / 60);
-    let seconds = time - minutes * 60;
-    return `${minutes}:${seconds < 10 ? `0${seconds}` : seconds}`;
+    if (!("" + time).includes(':')) {
+        let minutes = Math.floor(time / 60);
+        let seconds = time - minutes * 60;
+        return `${minutes}:${seconds < 10 ? `0${seconds}` : seconds}`;
+    }
+    return time;
 }
